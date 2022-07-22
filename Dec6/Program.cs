721dfbc92f60a520528a45f1fee3e02382914e70
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 15:36:54 -0700

    Add Day 6, Part 2

--- Dec6/Program.cs
+++ Dec6/Program.cs
@@ -2,6 +2,7 @@
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -22,4 +23,25 @@ static void Part1(string[] allLines)
 	//Add final Form
 	totalYes += currentForm.GetYesCount();
 	Console.WriteLine($"Part 1: {totalYes}");
+}
+
+static void Part2(string[] allLines)
+{
+	var currentForm = new CorrectedForm();
+	int totalYes = 0;
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			totalYes += currentForm.GetYesCount();
+			currentForm = new CorrectedForm();
+		}
+		else
+		{
+			currentForm.ProcessLine(thisLine);
+		}
+	}
+	//Add final Form
+	totalYes += currentForm.GetYesCount();
+	Console.WriteLine($"Part 2: {totalYes}");
 }
