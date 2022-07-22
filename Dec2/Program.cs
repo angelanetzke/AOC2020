ce5b3cd8ec570cd568fa9094d415a8cc56a281fe
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-21 20:22:47 -0700

    Add Day 2, Part 1

---
+++ Dec2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Dec2;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	int validCount = 0;
+	foreach (string thisLine in allLines)
+	{
+		if ((new PolicyAndPassword(thisLine)).IsValid())
+		{
+			validCount++;
+		}
+	}
+	Console.WriteLine($"Part 1: {validCount}");
+}
