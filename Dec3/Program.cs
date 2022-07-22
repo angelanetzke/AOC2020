f1e77bf33ae3bbbf55c7bb54f947264c79c5944b
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 11:12:33 -0700

    Add Day 3, Part 1

---
+++ Dec3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dec3;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var grid = new TreeGrid(allLines);
+	long treeCount = grid.CountTrees(3, 1);
+	Console.WriteLine($"Part 1: {treeCount}");
+}
