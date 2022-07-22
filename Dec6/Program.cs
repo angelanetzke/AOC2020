e45031e9fa9710a22be09533735c05bdc600462a
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 15:02:16 -0700

    Add Day 6, Part 1

---
+++ Dec6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dec6;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var currentForm = new Form();
+	int totalYes = 0;
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			totalYes += currentForm.GetYesCount();
+			currentForm = new Form();
+		}
+		else
+		{
+			currentForm.ProcessLine(thisLine);
+		}
+	}
+	//Add final Form
+	totalYes += currentForm.GetYesCount();
+	Console.WriteLine($"Part 1: {totalYes}");
+}
