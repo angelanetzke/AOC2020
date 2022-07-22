1973efb40282aeb9ee6147b460a41d3b78666fc1
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 12:42:31 -0700

    Add Day 4, Part 2

--- Dec4/Program.cs
+++ Dec4/Program.cs
@@ -2,6 +2,7 @@
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -24,6 +25,33 @@ static void Part1(string[] allLines)
 	}
 	//Make sure to process final Passport
 	if (!currentPassport.IsEmpty() && currentPassport.IsValid())
+	{
+		validCount++;
+	}
+	Console.WriteLine($"Part 1: {validCount}");
+}
+
+static void Part2(string[] allLines)
+{
+	var currentPassport = new Passport();
+	int validCount = 0;
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			if (currentPassport.IsDataValid())
+			{
+				validCount++;
+			}
+			currentPassport = new();
+		}
+		else
+		{
+			currentPassport.AddFields(thisLine);
+		}
+	}
+	//Make sure to process final Passport
+	if (!currentPassport.IsEmpty() && currentPassport.IsDataValid())
 	{
 		validCount++;
 	}
