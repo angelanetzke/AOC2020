6b7c119a149bdc84568d9de8c7829784f8939a54
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 14:18:38 -0700

    Add Day 5, Part 1

---
+++ Dec5/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dec5;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	int maxSeat = 0;
+	foreach(string thisLine in allLines)
+	{
+		var thisSeat = new Seat(thisLine);
+		maxSeat = Math.Max(maxSeat, thisSeat.GetSeatID());
+	}
+	Console.WriteLine($"Part 1: {maxSeat}");
+}
