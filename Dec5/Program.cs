13164bb7414246854b0caa85350f2e9e7db16594
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-22 14:26:03 -0700

    Add Day 5, Part 2

--- Dec5/Program.cs
+++ Dec5/Program.cs
@@ -2,6 +2,7 @@
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -12,4 +13,29 @@ static void Part1(string[] allLines)
 		maxSeat = Math.Max(maxSeat, thisSeat.GetSeatID());
 	}
 	Console.WriteLine($"Part 1: {maxSeat}");
+}
+static void Part2(string[] allLines)
+{
+	const int MAX_POSSIBLE = 127 * 8 + 7;
+	int maxSeat = 0;
+	int minSeat = MAX_POSSIBLE;
+	bool[] isSeatTaken = new bool[MAX_POSSIBLE];
+	foreach (string thisLine in allLines)
+	{
+		var thisSeat = new Seat(thisLine);
+		int thisSeatID = thisSeat.GetSeatID();
+		maxSeat = Math.Max(maxSeat, thisSeatID);
+		minSeat = Math.Min(minSeat, thisSeatID);
+		isSeatTaken[thisSeatID] = true;
+	}
+	int mySeatID = -1;
+	for (int i = minSeat + 1; i <= maxSeat + 1; i++)
+	{
+		if (isSeatTaken[i - 1] && !isSeatTaken[i] && isSeatTaken[i + 1])
+		{
+			mySeatID = i;
+			break;
+		}
+	}
+	Console.WriteLine($"Part 1: {mySeatID}");
 }
