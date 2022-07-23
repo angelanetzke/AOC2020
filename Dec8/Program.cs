ef71d0a5ea3d376248ba0353b5afbc6d2a95fae6
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-23 15:58:54 -0700

    Add Day 8, Part 2

--- Dec8/Program.cs
+++ Dec8/Program.cs
@@ -1,11 +1,18 @@
 ﻿using Dec8;
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
-Part1(allLines);
+var theBootCode = new BootCode(allLines);
+Part1(theBootCode);
+Part2(theBootCode);
 
-static void Part1(string[] allLines)
+static void Part1(BootCode bc)
 {
-	var theBootCode = new BootCode(allLines);
-	int acc = theBootCode.GetLastAcc();
+	int acc = bc.GetLastAcc();
 	Console.WriteLine($"Part 1: {acc}");
+}
+
+static void Part2(BootCode bc)
+{
+	int acc = bc.Debug();
+	Console.WriteLine($"Part 2: {acc}");
 }
