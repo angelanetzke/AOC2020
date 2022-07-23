d944a955eb9660c3829aef8b953739edc395a32f
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-23 15:15:19 -0700

    Add Day 8, Part 1

---
+++ Dec8/BootCode.cs
@@ -0,0 +1,56 @@
+﻿namespace Dec8
+{
+	internal class BootCode
+	{
+		private readonly string[] instructions;
+		private int ip = 0;
+		private int acc = 0;
+		private readonly bool[] hasBeenExecuted;
+
+		public BootCode(string[] instructions)
+		{
+			this.instructions = instructions;
+			hasBeenExecuted = new bool[instructions.Length];
+		}
+
+		public int GetLastAcc()
+		{
+			Execute();
+			return acc;
+		}
+
+		public void Execute()
+		{
+			if (ip >= instructions.Length)
+			{
+				return;
+			}
+			else if (hasBeenExecuted[ip])
+			{
+				return;
+			}
+			else
+			{
+				hasBeenExecuted[ip] = true;
+				string opcode = instructions[ip].Split(' ')[0];
+				int argument = int.Parse(instructions[ip].Split(' ')[1]);
+				switch(opcode)
+				{
+					case "acc":
+						acc += argument;
+						ip++;
+						break;
+					case "jmp":
+						ip += argument;
+						break;
+					case "nop":
+						ip++;
+						break;
+				}
+				Execute();
+			}
+		}
+
+
+	}
+}
---
+++ Dec8/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dec8;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var theBootCode = new BootCode(allLines);
+	int acc = theBootCode.GetLastAcc();
+	Console.WriteLine($"Part 1: {acc}");
+}
