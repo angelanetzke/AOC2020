af9a223f7976918419ffbbd11772795727a23c70
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-24 13:59:00 -0700

    Add Day 11, Part 1

---
+++ Dec11/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dec11;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var theRoom = new Room(allLines);
+	int occupiedCount = theRoom.CountOccupied();
+	Console.WriteLine($"Part 1: {occupiedCount}");
+}
