e66fbb51a9c7a115b60aed7fb7decc8cbbd976b9
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-23 17:01:34 -0700

    Add Day 9, Part 1

---
+++ Dec9/Program.cs
@@ -0,0 +1,41 @@
+﻿var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var numbers = new long[allLines.Length];
+	for (int i = 0; i < allLines.Length; i++)
+	{
+		numbers[i] = long.Parse(allLines[i]);
+	}
+	int checkIndex = 25;	
+	while(checkIndex < numbers.Length)
+	{
+		bool isValid = false;
+		for (int i = checkIndex - 25; i <= checkIndex - 2; i++)
+		{
+			for (int j = checkIndex - 24; j <= checkIndex - 1; j++)
+			{
+				if (numbers[i] + numbers[j] == numbers[checkIndex])
+				{
+					isValid = true;
+					break;
+				}
+			}
+			if (isValid)
+			{
+				break;
+			}
+		}
+		if (isValid)
+		{
+			checkIndex++;
+		}
+		else
+		{
+			long invalidNumber = numbers[checkIndex];
+			Console.WriteLine($"Part 1: {invalidNumber}");
+			return;
+		}		
+	}
+}
