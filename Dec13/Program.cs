ad8cfc456408fadfc10dc8259402b623f4c879c8
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-26 15:35:54 -0700

    Add Day 13, Part 1

---
+++ Dec13/Program.cs
@@ -0,0 +1,35 @@
+﻿var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	int startTime = int.Parse(allLines[0]);
+	var buses = allLines[1].Split(',');
+	int earliestBusNumber = -1;
+	int earliestBusTime = int.MaxValue;
+	foreach (string thisBusString in buses)
+	{
+		if (thisBusString != "x")
+		{
+			int thisBusNumber = int.Parse(thisBusString);
+			if (startTime % thisBusNumber == 0)
+			{
+				earliestBusNumber = thisBusNumber;
+				earliestBusTime = startTime;
+				break;
+			}
+			else
+			{
+				int thisNextBusTime = thisBusNumber * (startTime / thisBusNumber + 1);
+				Console.WriteLine($"{thisNextBusTime} {thisBusNumber}");
+				if (thisNextBusTime < earliestBusTime)
+				{
+					earliestBusNumber = thisBusNumber;
+					earliestBusTime = thisNextBusTime;
+				}
+			}
+		}
+	}
+	int part1Answer = earliestBusNumber * (earliestBusTime - startTime);
+	Console.WriteLine($"Part 1: {part1Answer}");
+}
