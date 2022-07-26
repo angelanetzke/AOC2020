3b1726c0c59e0dd59d75958586cec9012ea610da
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-26 12:40:30 -0700

    Add Day 12, Part 1

---
+++ Dec12/Program.cs
@@ -0,0 +1,16 @@
+﻿using Dec12;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var theFerry = new Ferry();
+	foreach(string thisLine in allLines)
+	{
+		theFerry.Next(thisLine);
+	}
+	int[] location = theFerry.GetCurrentLocation();
+	int distance = Math.Abs(location[0]) + Math.Abs(location[1]);
+	Console.WriteLine($"Part 1: {distance}");
+}
