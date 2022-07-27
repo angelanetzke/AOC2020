d1c4aff6643e6d35ff35a970130a03e3ae1fc2f0
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-27 15:23:53 -0700

    Add Day 16, Part 1

---
+++ Dec16/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dec16;
+using System.Text.RegularExpressions;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	List<string> rules = new();
+	var ruleRegex = new Regex(@"^[a-z ]+: \d+-\d+ or \d+-\d+$");
+	var ticketRegex = new Regex(@"^[\d+,]+$");
+	int errorRate = 0;
+	foreach (string thisLine in allLines)
+	{
+		if (ruleRegex.IsMatch(thisLine))
+		{
+			rules.Add(thisLine);
+		}
+		if (ticketRegex.IsMatch(thisLine))
+		{
+			errorRate += (new Ticket(thisLine, rules)).GetErrorRate();
+		}
+	}
+	Console.WriteLine($"Part 1: {errorRate}");
+}
---
+++ Dec16/Ticket.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dec16
+{
+	internal class Ticket
+	{
+		int[] fieldValues;
+		List<string> rules;
+
+		public Ticket(string ticketData, List<string> rules)
+		{
+			fieldValues = Array.ConvertAll(ticketData.Split(','), x => int.Parse(x));
+			this.rules = rules;
+		}
+
+		public int GetErrorRate()
+		{
+			int errorRate = 0;
+			foreach (int thisFieldValue in fieldValues)
+			{
+				bool isValid = false;
+				foreach (string thisRule in rules)
+				{
+					if (Validate(thisFieldValue, thisRule))
+					{
+						isValid = true;
+						break;
+					}
+				}
+				if (!isValid)
+				{
+					errorRate += thisFieldValue;
+				}
+			}
+			return errorRate;
+		}
+
+		private static bool Validate(int value, string rule)
+		{
+			//departure location: 41-525 or 538-968
+			var ruleRegex = new Regex(@"^[a-z ]+: (?<min1>\d+)-(?<max1>\d+) or (?<min2>\d+)-(?<max2>\d+)$");
+			int min1 = int.Parse(ruleRegex.Match(rule).Groups["min1"].Value);
+			int max1 = int.Parse(ruleRegex.Match(rule).Groups["max1"].Value);
+			int min2 = int.Parse(ruleRegex.Match(rule).Groups["min2"].Value);
+			int max2 = int.Parse(ruleRegex.Match(rule).Groups["max2"].Value);
+			return (min1 <= value && value <= max1) || (min2 <= value && value <= max2);
+
+		}
+
+
+	}
+}
