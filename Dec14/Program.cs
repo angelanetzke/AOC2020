1638ba5b20eb679eddd7a5eb1d9e0d34235cec3c
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-27 11:51:04 -0700

    Add DAy 14, Part 1

---
+++ Dec14/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dec14;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var theMemory = new Memory();
+	foreach (string thisLine in allLines)
+	{
+		theMemory.Next(thisLine);
+	}
+	long sum = theMemory.GetSum();
+	Console.WriteLine($"Part 1: {sum}");
+}
