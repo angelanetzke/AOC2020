869028da7350d09247049792ba62f31812d1e27e
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-28 11:22:46 -0700

    Add Day 18, Part 2

--- Dec18/Program.cs
+++ Dec18/Program.cs
@@ -2,6 +2,7 @@
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -9,6 +10,16 @@ static void Part1(string[] allLines)
 	foreach (string thisLine in allLines)
 	{
 		sum += Evaluate(thisLine);
+	}
+	Console.WriteLine($"Part 1: {sum}");
+}
+
+static void Part2(string[] allLines)
+{
+	long sum = 0;
+	foreach (string thisLine in allLines)
+	{
+		sum += Evaluate2(thisLine);
 	}
 	Console.WriteLine($"Part 1: {sum}");
 }
@@ -49,6 +60,60 @@ static long Evaluate(string expression)
 				value *= Evaluate(components[i]);
 				i++;
 			}
+		}
+	}
+	return value;
+}
+
+static long Evaluate2(string expression)
+{
+	if ((new Regex(@"^\d+$")).IsMatch(expression))
+	{
+		return long.Parse(expression);
+	}
+	List<string> components = new();
+	string[]? nextComponent = GetNextComponent(expression, 0);
+	while (nextComponent != null)
+	{
+		components.Add(nextComponent[0]);
+		nextComponent = GetNextComponent(expression, int.Parse(nextComponent[1]));
+	}
+	while (components.Contains("+"))
+	{
+		var newComponents = new List<string>();
+		int i = 0;
+		while (i < components.Count)
+		{
+			if (i == components.Count - 1)
+			{
+				newComponents.Add(components[i]);
+				i++;
+			}
+			else if (components[i + 1] == "+")
+			{
+				newComponents.Add((Evaluate2(components[i]) + Evaluate2(components[i + 2])).ToString());
+				i += 3;
+			}
+			else
+			{
+				newComponents.Add(components[i]);
+				i++;
+				newComponents.Add(components[i]);
+				i++;
+			}
+		}
+		components = newComponents;
+	}
+	long value = 0;
+	for (int i = 0; i < components.Count; i += 2)
+	{
+		if (i == 0)
+		{
+			value = Evaluate2(components[i]);
+		}
+		else
+		{
+			value *= Evaluate2(components[i]);
 		}
 	}
 	return value;
