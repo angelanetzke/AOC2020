083ce4030b1985ea033edc00fa02e34023714b0e
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-28 15:37:47 -0700

    Add Day 20, Part 1

---
+++ Dec20/Program.cs
@@ -0,0 +1,45 @@
+﻿using Dec20;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var tiles = new List<Tile>();
+	var thisTileData = new List<string>();
+	foreach(string thisLine in allLines)
+	{
+		if (thisLine.Length == 0)
+		{
+			tiles.Add(new Tile(thisTileData));
+			thisTileData = new();
+		}
+		else
+		{
+			thisTileData.Add(thisLine);
+		}
+	}
+	if (thisTileData.Count > 0)
+	{
+		tiles.Add(new Tile(thisTileData));
+	}
+	for (int i = 0; i < tiles.Count - 1; i++)
+	{
+		for (int j = i + 1; j < tiles.Count; j++)
+		{
+			if (i != j)
+			{
+				tiles[i].Connect(tiles[j]);
+			}
+		}
+	}
+	long product = 1L;
+	foreach (Tile thisTile in tiles)
+	{
+		if (thisTile.GetNeighborCount() == 2)
+		{
+			product *= thisTile.GetID();
+		}
+	}
+	Console.WriteLine($"Part 1: {product}");
+}
---
+++ Dec20/Tile.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dec20
+{
+	internal class Tile
+	{
+		private static readonly int SIZE = 10;
+		private readonly string[] data = new string[SIZE];
+		private readonly string[] borders = new string[4];
+		private readonly int id;
+		private readonly HashSet<Tile> neighbors = new();
+
+		public Tile(List<string> inputLines)
+		{
+			id = int.Parse(new Regex(@"Tile (?<id>\d+):").Match(inputLines[0]).Groups["id"].Value);
+			for (int i = 1; i < inputLines.Count; i++)
+			{
+				data[i - 1] = inputLines[i];
+				if (i == 1)
+				{
+					borders[0] = inputLines[i];
+				}
+				else if (i == inputLines.Count - 1)
+				{
+					borders[1] = inputLines[i];
+				}
+				borders[2] += inputLines[i][0];
+				borders[3] += inputLines[i][inputLines[i].Length - 1];
+			}
+		}
+
+		public int GetID()
+		{
+			return id;
+		}
+
+		public int GetNeighborCount()
+		{
+			return neighbors.Count;
+		}
+
+		public void Connect(Tile other)
+		{
+			for (int i = 0; i < borders.Length; i++)
+			{
+				for (int j = 0; j < borders.Length; j++)
+				{
+					if (borders[i] == other.borders[j])
+					{
+						neighbors.Add(other);
+						other.neighbors.Add(this);
+						return;
+					}
+					if (borders[i] == new string(other.borders[j].Reverse().ToArray()))
+					{
+						neighbors.Add(other);
+						other.neighbors.Add(this);
+						return;
+					}
+				}
+			}
+		}
+
+
+
+
+
+
+
+
+	}
+}
