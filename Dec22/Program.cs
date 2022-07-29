a109b406fb884a8f77732ec386a4441198bf9921
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-29 12:21:31 -0700

    Add Day 22, Part 2

--- Dec22/Program.cs
+++ Dec22/Program.cs
@@ -2,10 +2,18 @@
 
 var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
 	var theGame = new Game(allLines);
-	int finalScore = theGame.GetFinalWinnerScore();
+	int finalScore = theGame.Play();
+	Console.WriteLine($"Part 1: {finalScore}");
+}
+
+static void Part2(string[] allLines)
+{
+	var theGame = new Game(allLines);
+	int finalScore = int.Parse(theGame.PlayRecursive()[0]);
 	Console.WriteLine($"Part 1: {finalScore}");
 }
