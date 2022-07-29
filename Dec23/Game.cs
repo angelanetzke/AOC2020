cf0d9317740abd28142fedce693880e20374b46c
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-29 13:27:00 -0700

    Add Day 23, Part 1

---
+++ Dec23/Game.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+
+namespace Dec23
+{
+	internal class Game
+	{
+		private Cup current;
+		private readonly int size;
+
+		public Game(string start)
+		{
+			size = start.Length;
+			current = new Cup(int.Parse(start[0].ToString()));
+			Cup cursor = current;
+			for (int i = 1; i < start.Length; i++)
+			{
+				var newCup = new Cup(int.Parse(start[i].ToString()));
+				cursor.SetNext(newCup);
+				cursor = newCup;
+			}
+			cursor.SetNext(current);
+		}
+
+		public void Play(int rounds)
+		{
+			List<int> removedCupLabels = new();
+			for (int turn = 0; turn < rounds; turn++)
+			{
+				removedCupLabels.Clear();
+				var cursor = current;
+				var removedStart = current.GetNext();
+				for (int i = 0; i < 3; i++)
+				{
+					cursor = cursor.GetNext();
+					removedCupLabels.Add(cursor.GetLabel());
+				}
+				var removedEnd = cursor;
+				cursor = cursor.GetNext();
+				int destinationLabel = current.GetLabel() - 1;
+				if (destinationLabel < 1)
+				{
+					destinationLabel = size;
+				}
+				while (removedCupLabels.Contains(destinationLabel))
+				{
+					destinationLabel--;
+					if (destinationLabel < 1)
+					{
+						destinationLabel = size;
+					}
+				}
+				var destinationCup = current;
+				while (destinationCup.GetLabel() != destinationLabel)
+				{
+					destinationCup = destinationCup.GetNext();
+				}
+				var afterDestination = destinationCup.GetNext();
+				destinationCup.SetNext(removedStart);
+				removedEnd.SetNext(afterDestination);
+				current.SetNext(cursor);
+				current = current.GetNext();
+			}
+		}
+
+		public string GetLabels()
+		{
+			Cup? cursor = current;
+			while (cursor != null && cursor.GetLabel() != 1)
+			{
+				cursor = cursor.GetNext();
+			}
+			var builder = new StringBuilder();
+			for (int i = 1; i <= size - 1; i++)
+			{
+				cursor = cursor.GetNext();
+				builder.Append(cursor.GetLabel());
+			}
+			return builder.ToString();
+		}
+
+	}
+}
---
+++ Dec23/Program.cs
@@ -0,0 +1,12 @@
+﻿using Dec23;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var theGame = new Game(allLines[0]);
+	theGame.Play(100);
+	string labels = theGame.GetLabels();
+	Console.WriteLine($"Part 1: {labels}");
+}
