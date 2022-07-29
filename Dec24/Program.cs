6c62fd6f66b952cf9521ff1adf6f5e1eab8d6ae0
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-29 14:22:50 -0700

    Add Day 24, Part 1

---
+++ Dec24/Program.cs
@@ -0,0 +1,71 @@
+﻿using Dec24;
+
+var allLines = System.IO.File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var tiles = new Dictionary<Tile, bool>();
+	foreach (string thisLine in allLines)
+	{
+		int x = 0;
+		int y = 0;
+		int cursor = 0;
+		while (cursor < thisLine.Length)
+		{
+			if (thisLine[cursor] == 'e')
+			{
+				x += 10;
+				cursor++;
+			}
+			else if (thisLine[cursor] == 's')
+			{
+				cursor++;
+				if (thisLine[cursor] == 'e')
+				{
+					x += 5;
+					y += 5;
+					cursor++;
+				}
+				else if (thisLine[cursor] == 'w')
+				{
+					x -= 5;
+					y += 5;
+					cursor++;
+				}
+			}
+			else if (thisLine[cursor] == 'w')
+			{
+				x -= 10;
+				cursor++;
+			}
+			else if (thisLine[cursor] == 'n')
+			{
+				cursor++;
+				if (thisLine[cursor] == 'e')
+				{
+					x += 5;
+					y -= 5;
+					cursor++;
+				}
+				else if (thisLine[cursor] == 'w')
+				{
+					x -= 5;
+					y -= 5;
+					cursor++;
+				}
+			}
+		}
+		var thisTile = new Tile(x, y);
+		if (tiles.TryGetValue(thisTile, out bool isBlack))
+		{
+			tiles[thisTile] = !isBlack;
+		}
+		else
+		{
+			tiles[thisTile] = true;
+		}
+	}
+	int blackCount = tiles.Values.Count(thisBool => thisBool);
+	Console.WriteLine($"Part 1: {blackCount}");
+}
---
+++ Dec24/Tile.cs
@@ -0,0 +1,36 @@
+﻿namespace Dec24
+{
+	internal class Tile
+	{
+		private readonly int x;
+		private readonly int y;
+
+		public Tile(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			if (obj == null)
+			{
+				return false;
+			}
+			if (obj is Tile other)
+			{
+				return x == other.x && y == other.y;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return (x + y).GetHashCode();
+		}
+
+	}
+}
