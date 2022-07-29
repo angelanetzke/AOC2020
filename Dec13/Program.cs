9112fedfbbf74d6cf6c5fbed7e27142f010d1b28
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2022-07-28 19:35:42 -0700

    Add Day 13, Part 2

--- Dec13/Program.cs
+++ Dec13/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = System.IO.File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 static void Part1(string[] allLines)
 {
@@ -21,7 +22,6 @@ static void Part1(string[] allLines)
 			else
 			{
 				int thisNextBusTime = thisBusNumber * (startTime / thisBusNumber + 1);
-				Console.WriteLine($"{thisNextBusTime} {thisBusNumber}");
 				if (thisNextBusTime < earliestBusTime)
 				{
 					earliestBusNumber = thisBusNumber;
@@ -32,4 +32,54 @@ static void Part1(string[] allLines)
 	}
 	int part1Answer = earliestBusNumber * (earliestBusTime - startTime);
 	Console.WriteLine($"Part 1: {part1Answer}");
+}
+
+static void Part2(string[] allLines)
+{
+	var buses = allLines[1].Split(',');
+	var divisors = new List<long>();
+	var remainders = new List<long>();
+	for (int i = 0; i < buses.Length; i++)
+	{
+		if (buses[i] != "x")
+		{
+			divisors.Add(long.Parse(buses[i]));
+			remainders.Add(long.Parse(buses[i]) - i);
+		}
+	}
+	long part2Answer = ChineseRemainderTheorem(divisors, remainders);
+	Console.WriteLine($"Part 2: {part2Answer}");
+}
+
+static long ChineseRemainderTheorem(List<long> divisors, List<long> remainders)
+{
+	long product = divisors.Aggregate((x1, x2) => x1 * x2);
+	var n = new long[divisors.Count];
+	for (int i = 0; i < n.Length; i++)
+	{
+		n[i] = product / divisors[i];
+	}
+	var x = new long[n.Length];
+	for (int i = 0; i < x.Length; i++)
+	{
+		x[i] = GetInverse(n[i], divisors[i]);
+	}
+	long sum = 0;
+	for (int i = 0; i < remainders.Count; i++)
+	{
+		sum += remainders[i] * n[i] * x[i];
+	}
+	return sum % product;
+}
+
+static long GetInverse(long n, long divisor)
+{
+	long factor = 1;
+	long checkValue = factor * n % divisor;
+	while (checkValue != 1)
+	{
+		factor++;
+		checkValue = factor * n % divisor;
+	}
+	return factor;
 }
